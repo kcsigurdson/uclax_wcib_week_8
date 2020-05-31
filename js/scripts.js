520151c5f57cf405254f3cb2040905d54812a849
kcsigurdson <katiecsigurdson@gmail.com>
2020-05-30 17:15:55 -0700

    Aweseom App code

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
 jQuery(function() {
-	
+
+	$('#download_button').mouseover(function () {
+		$('#apple_img').attr('src', 'images/blackapple.png');
+	});
+
+	$('#download_button').mouseout(function () {
+		$('#apple_img').attr('src', 'images/apple.png');
+	});
+
 });
